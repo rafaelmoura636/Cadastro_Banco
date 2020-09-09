f1d3aef40ee0a1ea2739e043be31b507005a7f7e
Rafael Moura <rafinha_moura_11@hotmail.com>
2020-09-09 10:34:57 -0300

    Adicionado projeto completo

---
+++ src/program/aplication.java
@@ -0,0 +1,54 @@
+package program;
+
+import java.util.Scanner;
+
+import entities.Account;
+
+public class aplication {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		Account account;
+		
+		System.out.print("Enter account number: ");
+		int number = sc.nextInt();
+		System.out.print("Enter account holder: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Id there an initial deposit (y/n): ");
+		char response = sc.next().charAt(0);
+		
+		if (response == 'y') {
+			System.out.print("Enter an initial deposit value: ");
+			double initialDeposit = sc.nextDouble();
+			account = new Account(number, holder, initialDeposit);
+			
+		}
+		else {
+			account = new Account(number, holder);
+			
+		}
+		
+		System.out.println();
+		System.out.println("Account data: ");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a deposit value: ");
+		double depositValue = sc .nextDouble();
+		account.deposit(depositValue);
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Enter a withdrow value: ");
+		double withdrowValue = sc .nextDouble();
+		account.withdraw(withdrowValue);
+		System.out.println(account);
+		
+		
+		sc.close();
+		
+
+	}
+
+}
